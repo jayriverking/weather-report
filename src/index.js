25f74a047d70325c534ba8a990d8410e5cb625f7
Jay King <jayriverking@gmail.com>
2023-06-08 14:08:26 -0400

    add color change for temp up & down buttons

--- src/index.js
+++ src/index.js
@@ -9,25 +9,51 @@ console.log('curret temp', number.innerText);
 const incrementNum = () => {
     number.innerText = numInt++;
 }
-// decrementNum function
+// color changing function
+const colorTemp = (number) => {
+    let numInt = parseFloat(number.innerText);
+    if (numInt > 80){
+        number.style.color = "red";
+    }
+    else if (numInt > 69){
+        number.style.color = "orange";
+    }
+    else if (numInt > 59){
+        number.style.color = "yellow";
+    }
+    else if (numInt > 49){
+        number.style.color = "green";
+    }
+    else if (numInt > -100) {
+        number.style.color = "teal";
+        // number.style.backgroundColor = "#f4cccc";
+    }
+}
 
 upButton.addEventListener("click", () => {
     console.log(number);
     incrementNum();
+    colorTemp(number);
 });
+
+
+// colorTemp(number);
 
 // // upButton.addEventListener("click", (incrementNum));
 
 const decrementNum = () => {
-  number.innerText = numInt--;
+    number.innerText = numInt--;
 }
 
 downButton.addEventListener("click", () => {
-  console.log(number);
-  decrementNum()
+    console.log(number);
+    decrementNum()
+    colorTemp(number)
 });
 // downButton -- add event listender
 
 // changing text color: 
     // checktemperature 
         // document.getElementById("myH1's Id").style.color = "red";
+
+// Window.onload = colorTemp();
